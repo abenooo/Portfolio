25db8a1eda04234213bbe04c9e285dccdcd3aa1b
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-04-17 05:23:39 -0700

    Clean unused code

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import Navbar from "@/components/navbar"
 export default function Page() {
   return (
-    <div className="flex min-h-svh items-center justify-center">
+    <div>
       <Navbar />
     </div>
   )
