4dd045dc8eaef89bfed6b3af2b79b305e36e9c03
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-04-17 07:26:56 -0700

    Import footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button"
 import Navbar from "@/components/navbar"
+import Footer from "@/components/Footer"
 export default function Page() {
   return (
     <div>
       <Navbar />
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      <Footer />
     </div>
   )
 }
