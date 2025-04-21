057c199ade6c6d1d5962413329fe305f1e76e598
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-04-21 12:13:37 -0700

    Rename section ID from "about" to "experience" in AboutMe component for improved semantic clarity.

--- components/About.tsx
+++ components/About.tsx
@@ -238,7 +238,7 @@ export default function AboutMe() {
   }
 
   return (
-    <section id="about" className="py-20 bg-black">
+    <section id="experience" className="py-20 bg-black">
       <div className="max-w-7xl mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div className="md:col-span-1">
