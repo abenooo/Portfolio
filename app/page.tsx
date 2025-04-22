9afaf853342261a6c66dd154185fa8bef8f04701
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-04-22 04:38:30 -0700

    Add Project component to the main page for enhanced portfolio display; integrates seamlessly with existing layout.

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import Navbar from "@/components/navbar"
 import Footer from "@/components/Footer"
 import AboutMe from "@/components/About"
 import Services from "@/components/services"
-
+import Project from "@/components/Project"
 export default function Page() {
   return (
     <div>
       <Navbar />
       <Services/>
       <AboutMe />
+      <Project/>
       <Footer />
     </div>
   )
